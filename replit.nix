bc51ad5b9024fde981e8bdb8d61d61a4c5a50894
Nothing <73119361+findme-19@users.noreply.github.com>
2023-06-23 19:49:48 +0700

    Update nix biar support run di replit

--- replit.nix
+++ replit.nix
@@ -1,15 +1,15 @@
 { pkgs }: {
     deps = [
+        pkgs.arcan.ffmpeg
         pkgs.nodejs-16_x
         pkgs.openssh_with_kerberos
         pkgs.nodePackages.typescript
         pkgs.nodePackages.pm2
-        pkgs.arcan.ffmpeg
+        pkgs.imagemagick
         pkgs.libwebp
         pkgs.git
         pkgs.python2
         pkgs.python310Packages.python
-        pkgs.imagemagick
         pkgs.libuuid
     ];
     env = {
